4a5f39b0f745b8b66c7c44650f4354e84292f152
Daniel <daniel-mcintyre@hotmail.com>
2022-11-28 12:48:14 -0500

    header component setup

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,19 @@
 
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <nav>
+      <div className="flex justify-between bg-slate-200">
+        <div className="nav-left-container">
+            <div className="company logo">E-Commm</div>
+        </div>
+        <div className=" flex nav-right-container">
+             <div className="cart">Cart</div>
+        <div className="login-logo">Login</div>
+        </div>
+      </div>
+    </nav>
+    </>
   )
 }
 
