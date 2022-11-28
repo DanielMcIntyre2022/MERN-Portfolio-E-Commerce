69ab0095982ad8068110d83edcc558438b893a2a
Daniel <daniel-mcintyre@hotmail.com>
2022-11-28 13:28:08 -0500

    added cart and login pages and setup react-router to connect to those pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
      <Header/>
      <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </Router>
     </>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -5,11 +6,17 @@ function Header() {
     <nav>
       <div className="flex justify-between bg-slate-200">
         <div className="nav-left-container">
-            <div className="company logo">E-Commm</div>
+            <div className="company logo">
+              <Link to="/">E-Commm</Link>
+              </div>
         </div>
         <div className=" flex nav-right-container">
-             <div className="cart">Cart</div>
-        <div className="login-logo">Login</div>
+             <div className="cart">
+                <Link to="/cart">Cart</Link>
+              </div>
+        <div className="login-logo">
+          <Link to="/login">Login</Link>
+          </div>
         </div>
       </div>
     </nav>
