fd306de7564bfa3daa651fb9002e89963e11b6eb
Daniel <daniel-mcintyre@hotmail.com>
2022-11-28 12:03:16 -0500

    TailWindCSS setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import './index.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-     
+     <Header/>
+     <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </div>
   );
 }
